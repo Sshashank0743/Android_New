e7e9686eb6d61ccff733c27ee3b2de1996ca1269
shashank shukla <53620609+Sshashank0743@users.noreply.github.com>
2023-07-24 14:15:20 +0530

    updated

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -4,39 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button button_1;
-    Button button_2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button_1 = findViewById(R.id.button_1);
-
-        button_2 = findViewById(R.id.button_2);
-
-        button_1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("Button1", "Clicked");
-            }
-        });
-
-//        button_2.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Log.d("Button2", "Clicked");
-//            }
-//        });
     }
 
-    public void bluePressed(View view){
-        Log.d("Button2", "Clicked");
+    public void showCustomToast(){
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_layout, findViewById(R.id.toast_root));
     }
 }
